15bba6aa2462d4541e5a7e346409e05bb0a030cf
Thomas Aldaitz <thomas.aldaitz@wildcodeschool.com>
2023-06-01 18:32:58 +0200

    Save the form and add a Delete button

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,6 +6,7 @@ use App\Entity\Contact;
 use App\Form\ContactType;
 use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -30,14 +31,30 @@ class ContactController extends AbstractController
     }
 
     #[Route('/contact/new', name: 'app_contact_create')]
-    public function create(): Response
+    public function create(Request $request, ContactRepository $contactRepository): Response
     {
         $contact = new Contact;
-
         $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $contactRepository->save($contact, true);
+            return $this->redirectToRoute('app_contact_list');
+        }
+        
+
         return $this->render('contact/new.html.twig', [
             'form' => $form
         ]);
+    }
+
+    #[Route('/contact/delete/{id<\d+>}', name: 'app_contact_delete', methods: ['POST'])]
+    public function delete(Contact $contact, Request $request, ContactRepository $contactRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$contact->getId(), $request->request->get('_token'))) {
+            $contactRepository->remove($contact, true);
+        }
+        return $this->redirectToRoute('app_contact_list');
     }
 
 }
