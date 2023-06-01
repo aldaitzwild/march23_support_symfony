2fd612bc6e2900d51db9762dacaf184eff6dcc7e
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-06-01 14:51:43 +0200

    Add a contact form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
+use App\Form\ContactType;
 use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,11 +21,23 @@ class ContactController extends AbstractController
         ]);
     }
 
-    #[Route('/contact/{id}', name: 'app_contact_show')]
+    #[Route('/contact/{id<\d+>}', name: 'app_contact_show')]
     public function show(Contact $contact): Response
     {
         return $this->render('contact/show.html.twig', [
             'contact' => $contact
         ]);
     }
+
+    #[Route('/contact/new', name: 'app_contact_create')]
+    public function create(): Response
+    {
+        $contact = new Contact;
+
+        $form = $this->createForm(ContactType::class, $contact);
+        return $this->render('contact/new.html.twig', [
+            'form' => $form
+        ]);
+    }
+
 }
